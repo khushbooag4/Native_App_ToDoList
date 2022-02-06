7f5ca684fa1272d7b39fd4003bb1c7fa2bf5f20f
Khushboo <kushbooagarwal3584@gmail.com>
2022-02-06 20:15:53 +0530

    Add Button Added

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View,TextInput, Keyboard } from 'react-native';
+import { KeyboardAvoidingView, TouchableOpacity } from 'react-native-web';
+import Task from './components/Task';
+import Customize from './components/Customize';
 
 export default function App() {
+  const [task,setTask] = useState();
+  const [taskItems , setTaskItems] = useState([]); //array the tasks
+
+  const handleChange = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems , task]);//present taskItem me task will be appended at last
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+     let itemCopy = {...taskItems};
+     itemCopy.splice(index,1);
+     setTaskItems(itemCopy);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        <Text style={styles.heading}>Today's Tasks</Text>
+        <View style={styles.tasks}>
+          <Task text = "Task 1" />
+          {taskItems.map( (item,index) => {
+             return (
+               <TouchableOpacity key = {index} onPress = { () => completeTask(index)} >
+                 <Task text={item} />
+               </TouchableOpacity>
+             );
+          })}
+        </View>
+      </View>
+  
+
+      <KeyboardAvoidingView 
+         behaviour = {Platform.OS === "ios" ? "padding" : "height"}
+         style = {styles.keyboard}
+         >
+           <TextInput style ={styles.input} placeholder={"Enter the Task"} value = {task} onChangeText={text => setTask(text)} />
+           <TouchableOpacity onPress = {() => handleChange()}>
+               <View style = {styles.addWrapper}>
+                   <Text style={styles.addText}>+</Text>
+               </View>
+           </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -13,8 +57,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
+  wrapper: {
+    paddingTop: 80,
+    paddingRight:20,
+    paddingHorizontal: 20,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  tasks: {
+    marginTop: 30,
+  },
+  keyboard: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 370,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#333",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 3,
+    paddingBottom:"11px",
+    marginRight :"30px"
+  },
+  addText : {
+    fontSize : 45,
+    color : "#ee2BB1",
+  }
 });
---
+++ components/Task.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+import { TouchableOpacity } from 'react-native-web';
+
+const Task = (props) => {
+    return (
+      <View style = {styles.taskWrapper}>
+          <View style = {styles.itemLeft}>
+             <View style = {styles.square}></View>
+             <Text style = {styles.Stext}>{props.text}</Text>
+          </View>
+          <View style = {styles.circular}></View>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  taskWrapper: {
+    backgroundColor: "#EEE",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    marginRight :20,
+    borderColor : "#222"
+  },
+  square: {
+    width: 24,
+    height: 24,
+    backgroundColor: "#55BCF6",
+    opacity: 0.4,
+    borderRadius: 5,
+    marginRight: 15,
+  },
+  itemLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flexWrap: "wrap",
+  },
+  Stext: {
+    fontSize : 18,
+    maxWidth: "80%",
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: "#55BCF6",
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
+export default Task;
